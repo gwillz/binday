ed3ac3df9cda1ec9c8cded656d2d277f3d2f19fa
gwillz <me@gwilyn.com>
2020-06-27 21:51:09 +0930

    Rework widget into components.

--- src/common/Widget.tsx
+++ src/common/Widget.tsx
@@ -19,29 +19,46 @@ function s(names: Record<string, any>) {
         .join(' ')
 }
 
-type Props = {
+type WidgetProps = {
     bin_week: string;
     bin_day: string;
 }
 
-export function Widget(props: Props) {
+export function Widget(props: WidgetProps) {
     return (
         <div class={style.widget}>
             <style>{css}</style>
             {DAYS.map(day => (
-                <div crank-key={day}
-                    class={s({
-                        [style.day]: true,
-                        [props.bin_week]: day === props.bin_day,
-                    })}>
-                    <span class={style.label}>{day.slice(0, 2)}</span>
-                    {day === props.bin_day && (
-                        <div class={style.icon}>
-                            <Raw value={bin} />
-                        </div>
-                    )}
-                </div>
+                <Day crank-key={day}
+                    highlight={day === props.bin_day}
+                    color={props.bin_week}
+                    title={day}
+                />
             ))}
+        </div>
+    )
+}
+
+type DayProps = {
+    highlight: boolean;
+    color: string;
+    title: string;
+}
+
+function Day(props: DayProps) {
+    return (
+        <div class={s({
+            [style.day]: true,
+            [props.color]: props.highlight,
+        })}>
+            <span class={style.label}>
+                {props.title.slice(0, 2)}
+            </span>
+            {props.highlight && (
+                <div class={style.icon}>
+                    <Raw value={bin} />
+                </div>
+            )}
         </div>
     )
 }
