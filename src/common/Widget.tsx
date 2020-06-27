3d4ab72913bbc24035a0851370e0e0f1c74ebb24
gwillz <me@gwilyn.com>
2020-06-27 16:39:47 +0930

    Added widget styles and bin svg.

--- src/common/Widget.tsx
+++ src/common/Widget.tsx
@@ -1,5 +1,23 @@
 
-import { createElement } from '@bikeshaving/crank';
+import { createElement, Raw } from '@bikeshaving/crank';
+import bin from './bin.svg';
+import style, {css} from './widget.css';
+
+const DAYS = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+];
+
+function s(names: Record<string, any>) {
+    return Object.entries(names)
+        .map(([name, test]) => test ? name : '')
+        .join(' ')
+}
 
 type Props = {
     bin_week: string;
@@ -8,9 +26,22 @@ type Props = {
 
 export function Widget(props: Props) {
     return (
-        <div>
-            <div>This week is: {props.bin_week}</div>
-            <div>Your bin day is: {props.bin_day}</div>
+        <div class={style.widget}>
+            <style>{css}</style>
+            {DAYS.map(day => (
+                <div crank-key={day}
+                    class={s({
+                        [style.day]: true,
+                        [props.bin_week]: day === props.bin_day,
+                    })}>
+                    <span class={style.label}>{day.slice(0, 2)}</span>
+                    {day === props.bin_day && (
+                        <div class={style.icon}>
+                            <Raw value={bin} />
+                        </div>
+                    )}
+                </div>
+            ))}
         </div>
     )
 }
